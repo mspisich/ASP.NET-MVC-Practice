79daf002cd23454d0fa61a4ec48ed848b7d380b8
mspisich <mike.spisich@gmail.com>
2017-04-26 11:15:18 -0400

    First Commit.

---
+++ Lab20/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab20.Models;
+
+namespace Lab20.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult Success(Register r)
+        {
+            ViewBag.Message = "Hello, " + r.FirstName + ". Your email is " + r.Email + ".";
+            return View();
+        }
+    }
+}
